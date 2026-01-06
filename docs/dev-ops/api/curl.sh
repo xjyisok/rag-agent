44fa9489bf08aaadfe4fa8dc80b06781ef4a3a51
xjy <2154675472@qq.com>
2026-01-06 18:22:55 +0800

    工程初始化

---
+++ docs/dev-ops/api/curl.sh
@@ -0,0 +1,7 @@
+curl http:/127.0.0.1:11434/api/generate \
+  -H "Content-Type: application/json" \
+  -d '{
+        "model": "deepseek-r1:1.5b",
+        "prompt": "1+1",
+        "stream": false
+      }'
