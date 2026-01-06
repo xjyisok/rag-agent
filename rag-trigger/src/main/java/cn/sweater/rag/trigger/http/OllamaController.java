082493a416f097faad1ea95e8fb584ded84d3cfc
xjy <2154675472@qq.com>
2026-01-06 19:37:04 +0800

    流式应答接口

---
+++ rag-trigger/src/main/java/cn/sweater/rag/trigger/http/OllamaController.java
@@ -0,0 +1,38 @@
+package cn.sweater.rag.trigger.http;
+
+import cn.sweater.rag.api.IAiService;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+@RestController()
+@CrossOrigin("*")
+@RequestMapping("/api/v1/ollama/")
+public class OllamaController implements IAiService {
+
+    @Resource
+    private OllamaChatClient chatClient;
+
+    /**
+     * http://localhost:8090/api/v1/ollama/generate?model=deepseek-r1:1.5b&message=1+1
+     */
+    @RequestMapping(value = "generate", method = RequestMethod.GET)
+    @Override
+    public ChatResponse generate(@RequestParam String model, @RequestParam String message) {
+        return chatClient.call(new Prompt(message, OllamaOptions.create().withModel(model)));
+    }
+
+    /**
+     * http://localhost:8090/api/v1/ollama/generate_stream?model=deepseek-r1:1.5b&message=hi
+     */
+    @RequestMapping(value = "generate_stream", method = RequestMethod.GET)
+    @Override
+    public Flux<ChatResponse> generateStream(@RequestParam String model, @RequestParam String message) {
+        return chatClient.stream(new Prompt(message, OllamaOptions.create().withModel(model)));
+    }
+
+}
